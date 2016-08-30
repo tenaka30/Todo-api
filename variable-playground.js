b9b37014243cbf3ea48aac4b48f9dde7abd16dc8
Davey Whyte <tenaka@fragfest.co.uk>
2016-08-30 13:11:05 +0100

    Add variable plaground area

---
+++ variable-playground.js
@@ -0,0 +1,30 @@
+// var person = {
+// 	name: 'Davey',
+// 	age: 20
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Davey',
+// 	// 	age: 43
+// 	// };
+
+// 	obj.age = 65;
+// }
+
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [15, 88];
+
+function addGrades (grades) {
+	grades.push(55);
+	debugger;
+
+	// grades = [12, 3, 99];
+
+}
+
+addGrades(grades);
+console.log(grades);
